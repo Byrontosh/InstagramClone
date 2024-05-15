8f4f13803835c9c20971004aed6557a6ef7e983d
BYRON LOARTE <by_tosh20@hotmail.com>
2024-05-15 12:02:28 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,60 @@
+
+
+
+# import webbrowser
+
+# print("Bienvenido, Byron")
+# dia = input("Ingrese un día de la semana: ")
+
+# match (dia):
+#     case "lunes":
+#         print("Hoy debes, hacer ejercicios")
+#     case "martes":
+#         print("Hoy debes, hacer las compras")
+#     case "miercoles":
+        
+#         print("Hoy debes, ver películas")
+#         print("¿Deseas que te recomiende un canal?")
+#         tipoWeb=input("Dime si o no:")
+#         if tipoWeb == "si":
+#             webbrowser.open_new_tab("https://www.disneyplus.com")
+
+#     case "jueves":
+#         print("Hoy debes, hacer deberes")
+#     case "viernes":
+#         print("Hoy debes, jugar fútbol")
+#     case _:
+#         print("No existe actividad para ese día")
+
+
+
+
+print("Instagram")
+
+user = input("Phone number, username or email")
+passowrd= input("Passowrd")
+
+if user == "Byron" and passowrd =="123":
+    print(f'Welcome {user}')
+    print("Loading your posts")
+else:
+    print("Sorry, your password was incorrect. Please double-check your password.")
+
+
+print("Sign up to see photos and videos from your friends.")
+input("Log in with Facebook")
+facebook=input("Yes or No")
+if  facebook =="si":
+    print("Welcome")
+else:
+    mobile=input("Mobile number or email")
+    name=input("Full name")
+    username=input("Username")
+    password=input("password")
+
+
+
+
+
+
+
